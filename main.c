87b23cc3d1d74b3f3aa762b8396b6ef1fce2ca54
pecezon <lopez.diego@cetys.edu.mx>
2025-11-05 21:00:36 -0800

    Map random word

--- main.c
+++ main.c
@@ -12,8 +12,15 @@ int main(void) {
     char *guessingWord = randomWord();
     printf("Palabra aleatoria: %s\n", guessingWord);
 
+    int *mapped = mapWord(guessingWord);
+    printf("Mapa de la palabra:\n");
+    for (int i = 0; i < 26; i++) {
+        printf("%c: %d\n", 'A' + i, mapped[i]);
+    }
+
     return 0;
 }
+
 
 int randomNumber(int size) {
     return rand() % size;
@@ -32,3 +39,21 @@ char *randomWord() {
 
     return words[randIdx];
 }
+
+int[] mapWord(char[] word) {
+    int mappedWord[26];
+
+    for (int i = 0; i < 26; i++) {
+        mappedWord[i] = 0;
+    }
+
+    for (int i = 0; word[i] != '\0'; i++) {
+        int index = word[i] - 'A';
+        if (index >= 0 && index < 26) {
+            mappedWord[index]++;
+        }
+    }
+
+    return mappedWord;
+}
+
