420c01c1292a8e806389bf8e21e7766b2c6840a7
Ramiro Cruz Labrada <rcrzlbrd@gmail.com>
2025-11-10 20:44:52 -0800

    Implement the function to verify the guess of the user and the guessing word

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 char *randomWord();
 int randomNumber(int size);
 int *mapWord(char *word);
+void guessCheck(char *guess, char *word);
 
 int main(void) {
     srand(time(NULL));  // inicializar random una sola vez
@@ -22,6 +23,21 @@ int main(void) {
     free(mappedSystemWord);
 
     // Make logic for user attempts
+
+    char word[5] = {'W', 'A', 'Z', 'A', 'A'};
+    char guess[5] = {'A', 'A', 'A', 'A', 'A'};
+    for (int i = 0; i < 5; i++){
+        printf("%c ", word[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < 5; i++){
+        printf("%c ", guess[i]);
+    }
+
+    printf("\n");
+    guessCheck(guess, word);
 
     return 0;
 }
@@ -56,3 +72,33 @@ int *mapWord(char *word) {
     return mappedWord;
 }
 
+void guessCheck(char *guess, char *word){
+    int *mapped = mapWord(word), result[5];
+
+    //green
+    for (int i = 0; i < 5; i++) {
+        if (guess[i] == word[i]) {
+            result[i] = 2;   
+            mapped[guess[i] - 'A']--;
+        }
+    }
+
+    //yellow and black
+    for (int i = 0; i < 5; i++){
+        if (result[i] != 2){
+            int index = guess[i] - 'A';
+            if (mapped[index] > 0){
+                result[i] = 1;
+                mapped[index]--;
+            }
+            else {
+                result[i] = 0;
+            }
+        }
+    }
+
+    //showing 
+    for (int i = 0; i < 5; i++){
+        printf("%d ", result[i]);
+    }
+}
